1166cde4678c736a3cb226d4434446ffbc49d723
Andrew Dunai <a@dun.ai>
2022-06-18 00:34:07 +0300

    Update conversation example with more functionality

--- examples/conversation_with_callbacks/main.go
+++ examples/conversation_with_callbacks/main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	tm "github.com/and3rson/telemux/v2"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const (
-	SearchProductMessage = "Search product by ID"
+	StartShoppingMessage = "Start shopping"
+	CheckoutMessage      = "Checkout"
 )
+
+type Cart map[string]bool
 
 type Product struct {
 	Sku   string
@@ -27,11 +31,29 @@ var storage = map[string]Product{
 		Image: "https://picsum.photos/id/252/600/400",
 		Price: 89,
 	},
+	"p02": {
+		Sku:   "a88xn",
+		Title: "Another cool product",
+		Image: "https://picsum.photos/id/253/600/400",
+		Price: 1337,
+	},
+	"p03": {
+		Sku:   "a89xn",
+		Title: "The best product",
+		Image: "https://picsum.photos/id/254/600/400",
+		Price: 150000,
+	},
 }
 
 var startKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton(SearchProductMessage),
+		tgbotapi.NewKeyboardButton(StartShoppingMessage),
+	),
+)
+
+var checkoutKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(CheckoutMessage),
 	),
 )
 
@@ -51,8 +73,7 @@ func main() {
 			tm.IsCommandMessage("start"),
 			func(u *tm.Update) {
 
-				msg := tgbotapi.NewMessage(u.Message.Chat.ID, u.Message.Text)
-				startKeyboard.OneTimeKeyboard = true
+				msg := tgbotapi.NewMessage(u.Message.Chat.ID, "Welcome to our shop!")
 				msg.ReplyMarkup = startKeyboard
 
 				bot.Send(msg)
@@ -63,25 +84,60 @@ func main() {
 			tm.NewLocalPersistence(),
 			tm.StateMap{
 				"": {
-					tm.NewHandler(tm.And(tm.IsMessage(), tm.HasRegex("^"+SearchProductMessage)), func(u *tm.Update) {
+					tm.NewHandler(tm.And(tm.IsMessage(), tm.HasRegex("^"+StartShoppingMessage)), func(u *tm.Update) {
 						msg := tgbotapi.NewMessage(
 							u.Message.Chat.ID,
-							"Provide a product ID",
+							"Provide a product ID. You can type /cancel at any time to cancel the process.",
 						)
+						msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(false)
 
 						bot.Send(msg)
-						u.PersistenceContext.SetState("enter_productId")
+						u.PersistenceContext.SetState("enter_product_id")
+
+						// Initialize user cart if not initialized yet
+						if _, ok := u.PersistenceContext.GetData()["cart"]; !ok {
+							u.PersistenceContext.PutDataValue("cart", make(Cart))
+						}
 					}),
 				},
-				"enter_productId": {
+				"enter_product_id": {
+					tm.NewHandler(tm.And(tm.IsMessage(), tm.HasRegex("^"+CheckoutMessage)), func(u *tm.Update) {
+						cart := u.PersistenceContext.GetData()["cart"].(Cart)
+						u.PersistenceContext.ClearData()
+
+						var msg tgbotapi.MessageConfig
+
+						if len(cart) > 0 {
+							lines := []string{}
+							for id := range cart {
+								product := storage[id]
+								lines = append(lines, fmt.Sprintf("- %s (%d $)", product.Title, product.Price))
+							}
+
+							msg = tgbotapi.NewMessage(
+								u.Message.Chat.ID,
+								"Your has been recorded!\n\n"+strings.Join(lines, "\n")+"\n\nSee you again soon!",
+							)
+						} else {
+							msg = tgbotapi.NewMessage(
+								u.Message.Chat.ID,
+								"See you again soon!",
+							)
+						}
+
+						msg.ReplyMarkup = startKeyboard
+						bot.Send(msg)
+
+						u.PersistenceContext.SetState("")
+					}),
 					tm.NewHandler(tm.HasText(), func(u *tm.Update) {
 						bot.Send(tgbotapi.NewChatAction(u.Message.Chat.ID, tgbotapi.ChatTyping))
 
-						productId := u.Message.Text
+						productID := u.Message.Text
 
 						msg := tgbotapi.NewMessage(u.Message.Chat.ID, "")
 
-						product, ok := storage[productId]
+						product, ok := storage[productID]
 						if !ok {
 							msg.Text = "Product not found"
 							bot.Send(msg)
@@ -95,13 +151,17 @@ func main() {
 
 						share.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
 							tgbotapi.NewInlineKeyboardRow(
-								tgbotapi.NewInlineKeyboardButtonData("Add to cart", "buyProduct "+productId),
+								tgbotapi.NewInlineKeyboardButtonData("Add to cart", "add:"+productID),
 							),
 						)
 
 						bot.Send(share)
+
+						instructions := tgbotapi.NewMessage(u.Message.Chat.ID, "Type another product ID to search, or click checkout to finish.")
+						instructions.ReplyMarkup = checkoutKeyboard
+						bot.Send(instructions)
 					}),
-					tm.NewHandler(tm.And(tm.Not(tm.IsCommandMessage("back")), tm.Not(tm.IsCallbackQuery())), func(u *tm.Update) {
+					tm.NewHandler(tm.And(tm.Not(tm.IsCommandMessage("cancel")), tm.Not(tm.IsCallbackQuery())), func(u *tm.Update) {
 						bot.Send(tgbotapi.NewMessage(
 							u.Message.Chat.ID,
 							"ID only!",
@@ -110,29 +170,21 @@ func main() {
 				},
 			},
 			[]*tm.Handler{
-				tm.NewHandler(tm.IsCommandMessage("back"), func(u *tm.Update) {
+				tm.NewHandler(tm.IsCommandMessage("cancel"), func(u *tm.Update) {
 					u.PersistenceContext.ClearData()
 					u.PersistenceContext.SetState("")
 
-					msg := tgbotapi.NewMessage(u.Message.Chat.ID, u.Message.Text)
-
-					startKeyboard.OneTimeKeyboard = true
+					msg := tgbotapi.NewMessage(u.Message.Chat.ID, "See you again soon!")
 					msg.ReplyMarkup = startKeyboard
 
 					bot.Send(msg)
 				}),
-				// During the active conversation this callback handler will be invoked first
-				tm.NewHandler(tm.IsCallbackQuery(), func(u *tm.Update) {
-
+				// During the active conversation these callback handler will be invoked
+				// before the ones that are outside of this conversation.
+				tm.NewCallbackQueryHandler(`^add:(.+)`, nil, func(u *tm.Update) {
 					loadingMarkup := tgbotapi.NewInlineKeyboardMarkup(
 						tgbotapi.NewInlineKeyboardRow(
-							tgbotapi.NewInlineKeyboardButtonData("Loading...", "load"),
-						),
-					)
-
-					refreshMarkup := tgbotapi.NewInlineKeyboardMarkup(
-						tgbotapi.NewInlineKeyboardRow(
-							tgbotapi.NewInlineKeyboardButtonData("In cart", "return"),
+							tgbotapi.NewInlineKeyboardButtonData("Loading...", ""),
 						),
 					)
 
@@ -144,16 +196,63 @@ func main() {
 					))
 					bot.Send(tgbotapi.NewChatAction(u.CallbackQuery.Message.Chat.ID, tgbotapi.ChatTyping))
 
+					productID := u.Context["matches"].([]string)[1]
+					cart := u.PersistenceContext.GetData()["cart"].(Cart)
+					cart[productID] = true
+					u.PersistenceContext.PutDataValue("cart", cart)
+
+					refreshMarkup := tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(
+							tgbotapi.NewInlineKeyboardButtonData("Remove from cart", "remove:"+productID),
+						),
+					)
+
 					bot.Send(tgbotapi.NewEditMessageReplyMarkup(
 						u.CallbackQuery.Message.Chat.ID,
 						u.CallbackQuery.Message.MessageID,
 						refreshMarkup,
 					))
+
+					fmt.Printf("Cart: %v\n", cart)
+				}),
+				tm.NewCallbackQueryHandler(`^remove:(.+)`, nil, func(u *tm.Update) {
+					loadingMarkup := tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(
+							tgbotapi.NewInlineKeyboardButtonData("Loading...", ""),
+						),
+					)
+
+					bot.Send(tgbotapi.NewCallback(u.CallbackQuery.ID, "Refreshing..."))
+					bot.Send(tgbotapi.NewEditMessageReplyMarkup(
+						u.CallbackQuery.Message.Chat.ID,
+						u.CallbackQuery.Message.MessageID,
+						loadingMarkup,
+					))
+					bot.Send(tgbotapi.NewChatAction(u.CallbackQuery.Message.Chat.ID, tgbotapi.ChatTyping))
+
+					productID := u.Context["matches"].([]string)[1]
+					cart := u.PersistenceContext.GetData()["cart"].(Cart)
+					delete(cart, productID)
+					u.PersistenceContext.PutDataValue("cart", cart)
+
+					refreshMarkup := tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(
+							tgbotapi.NewInlineKeyboardButtonData("Add to cart", "add:"+productID),
+						),
+					)
+
+					bot.Send(tgbotapi.NewEditMessageReplyMarkup(
+						u.CallbackQuery.Message.Chat.ID,
+						u.CallbackQuery.Message.MessageID,
+						refreshMarkup,
+					))
+
+					fmt.Printf("Cart: %v\n", cart)
 				}),
 			},
 		)).
 		AddHandler(tm.NewHandler(tm.IsCallbackQuery(), func(u *tm.Update) {
-			callback := tgbotapi.NewCallback(u.CallbackQuery.ID, u.CallbackQuery.Data)
+			callback := tgbotapi.NewCallback(u.CallbackQuery.ID, "Cannot modify cart at this time")
 			if _, err := bot.Request(callback); err != nil {
 				log.Print(err)
 			}
